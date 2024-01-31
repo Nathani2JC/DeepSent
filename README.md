26b21c023e18621d3078726b07e7661ea605a0fc
Nathaniel <83274852+Nathani2JC@users.noreply.github.com>
2024-01-31 14:25:17 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# DeepSent
+## Project Description
+This project will analyze the user-uploaded music files based on the arousal-valence emotion model (aka [Circumplex model](https://en.wikipedia.org/wiki/Emotion_classification)). Arousal represents how intense or "stimulating" the music is to the human ears, ranging from a plain and relaxing feeling to a strong and arousing feeling. Valence here describes how delightful or how sad the music sounds like. This definition is narrower than the one presented in [wikipedia](https://en.wikipedia.org/wiki/Valence_(psychology)), but is easier for the machine learning models to classify. Besides, the project also implemented the functionalities of pace detection and music genre detection.<br/>
+Web Framework: [Flask](http://flask.pocoo.org/), [jQuery](https://jquery.com/)<br/>
+Skills Involved: HTML, CSS, javascript, python<br/>
+
+### Implmentation Details
+Pace, arousal and valence detection: the middle 50% of the music's raw data is splitted into 5-second frames, with a step of 0.5 seconds. Each frame is then splitted into smaller 25ms subframes that are then transformed into an array of MFCCs (Mel-frequency cepstral coefficients). Finally, the MFCC array, which is initially in matrix form, is flattened and fed into a 3-layer neural network regressor. The intention here is trying to let the neural nets capture the patterns in the changes of the MFCCs and relates the patterns with the emotions that the music represents. (an instinctive approach, more researches are needed)<br/>
+
+Genre detection: the middle 50% of the music's raw data is splitted into 20ms frames. From each of the frames, an vector of MFCC values is calculated. The mean vector and the upper part of the covariance matrix of the MFCC vectors are then taken and concatenated together into a single array, which is finally fed into a 6-layer neural network that outputs the numerical label for the predicted genre.<br/>
+
+Datasets: GTZAN Music Genre Dataset + music files collected from my own favorite playlists for added genres (East Asia Classical, Electronic, New Age and Soundtracks)   
+
+## Getting Started
+The project is hosted on the site: <del>mchen241.pythonanywhere.com</del> (Expired)<br/>
+Or you can download the repository and run app_main.py (For requirements that need to be installed, see "Prerequisites" below):
+```
+python app_main.py
+```
+The project will be running on localhost:5000/
+
+## Prerequisites
+```
+python>=2.7.12
+scikit-learn>=0.18rc2
+scipy>=0.17.0
+numpy>=0.11.0
+flask>=0.12.2
+pydub (with libav or ffmpeg)
+```
+
+## Author
+Natnael Bekal [nate](https://github.com/Nathani2JC) 
+
+## References
+1. Music Genre Classification, cs229.stanford.edu/proj2011/HaggbladeHongKao-MusicGenreClassification.pdf
+2. http://practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/
+3. http://www.ee.columbia.edu/ln/rosa/doc/HTKBook21/node54.html
+4. Wikipedia, https://www.wikipedia.org/
---
+++ static/scripts/script.js
@@ -0,0 +1,103 @@
+var upload_div_on = true;
+var analysis_div_on = false;
+var contact_div_on = false;
+var analysis_completed = true;
+
+function onLoad () {
+	$(document).ready (function () {
+		setHeadersOnClickEvents ();
+		$(".analysis-header").css ("color", "lightgray");
+		$(".alert-bar").hide ();
+		if (file_uploaded) {
+			$(".alert-bar").text ("Analyzing...");
+			$(".alert-bar").show ();
+			$(".upload-form input").prop ("disabled", true);
+			$.post (url="/analysis", function (data) {
+				// post request succeeds
+				data_parsed = JSON.parse (data);
+				$(".upload-filename").text (data_parsed["filename"]);
+				
+				$(".tempo-scale").val (data_parsed["pace_score"]);
+				$(".tempo-ratio-label").text ("Slow: " + data_parsed["pace_slow_ratio"].toFixed(2) + 
+					"%, Mid: " + data_parsed["pace_mid_ratio"].toFixed(2) + "%, Fast: " + data_parsed["pace_fast_ratio"].toFixed(2) + "%");
+
+				$(".arousal-scale").val (data_parsed["arousal_score"]);
+				$(".arousal-ratio-label").text ("Relaxing: " + data_parsed["arousal_relaxing_ratio"].toFixed (2) + "% ," + 
+					"Neutral: " + data_parsed["arousal_mid_ratio"].toFixed(2) + "% ," + 
+					"Intense: " + data_parsed["arousal_intense_ratio"].toFixed(2) + "%");
+
+				$(".valence-scale").val (data_parsed["valence_score"]);
+				$(".valence-ratio-label").text ("Sad: " + data_parsed["valence_sad_ratio"].toFixed(2) + "% ," + 
+					"Neutral: " + data_parsed["valence_neutral_ratio"].toFixed(2) + "% ," + 
+					"Happy: " + data_parsed["valence_happy_ratio"].toFixed(2) + "%");
+
+				$(".best-match").text (data_parsed["best_cand"]);
+				$(".sec-best-match").text (data_parsed["sec_best_cand"]);
+
+				$(".analysis-header").css ("color", "#66c2ff");
+				setAnalysisHeaderOnClickEvents ();
+				$(".analysis-body").slideDown ("slow", function () {
+					analysis_div_on = true;
+					$(".analysis-header").text ("Analysis Result  \u25b2");
+				});
+
+				$(".waveform-img").attr ("src", "/waveform_image?" + (new Date()).getTime());
+				$(".alert-bar").hide ()
+
+			}).fail (function () {
+				// post request fails
+				$(".alert-bar").text ("Unexpected error during analyzing the sound data");
+				$(".alert-bar").css ("background", "red");
+			}).always (function () {
+				file_uploaded = false;
+				$(".upload-form input").prop ("disabled", false);
+			});
+		}
+	});
+}
+
+function setHeadersOnClickEvents () {
+	$(document).on ("click", ".upload-header", function () {
+			$(".upload-body").slideToggle ("slow", function() {
+				upload_div_on = !upload_div_on;
+				if (upload_div_on) {
+					$(".upload-header").text ("Start with uploading a file  \u25b2");
+				} else {
+					$(".upload-header").text ("Start with uploading a file  \u25bc");
+				}
+			});
+		}
+	);
+
+	$(document).on ("click", ".contact-header", function () {
+		$(".contact-body").slideToggle ("slow", function () {
+			contact_div_on = !contact_div_on;
+			if (contact_div_on) {
+				$(".contact-header").text ("Contact Info  \u25b2");
+			} else {
+				$(".contact-header").text ("Contact Info  \u25bc");
+			}
+		});
+	});
+
+	$(".upload-form").one ("submit", function (event) {
+		$(".alert-bar").show ();
+		$(".alert-bar").text ("Uploading...");
+		$(".messages").hide ();
+		return true;
+	});
+}
+
+function setAnalysisHeaderOnClickEvents () {
+		$(document).on ("click", ".analysis-header", function () {
+			$(".analysis-body").slideToggle ("slow", function() {
+				analysis_div_on = !analysis_div_on;
+				if (analysis_div_on) {
+					$(".analysis-header").text ("Analysis Result  \u25b2");
+				} else {
+					$(".analysis-header").text ("Analysis Result  \u25bc");
+				}
+			});
+		}
+	);
+}
